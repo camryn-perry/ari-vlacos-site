09bf34da12072476d40c0d37b8fa65664d0a377d
Camryn Perry <camrynpaigeperry@gmail.com>
2024-03-17 14:18:30 -0400

    remove container components not needed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -126,13 +126,10 @@ function MobileNavigation(
             </div>
             <nav className='mt-6'>
               <ul className='-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300'>
-                <MobileNavItem href='/'>Home</MobileNavItem>
-                <MobileNavItem href='/Book'>Book</MobileNavItem>
+                <MobileNavItem href='/'>Videos</MobileNavItem>
                 <MobileNavItem href='/about'>About</MobileNavItem>
                 <MobileNavItem href='/work'>Work</MobileNavItem>
                 <MobileNavItem href='/flash'>Flash</MobileNavItem>
-                <MobileNavItem href='/faq'>FAQ</MobileNavItem>
-                <MobileNavItem href='/guest'>Guest</MobileNavItem>
               </ul>
             </nav>
           </Popover.Panel>
@@ -174,13 +171,10 @@ function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
   return (
     <nav {...props}>
       <ul className='flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10'>
-        <NavItem href='/'>Home</NavItem>
-        <NavItem href='/book'>Book</NavItem>
+        <NavItem href='/'>Videos</NavItem>
         <NavItem href='/about'>About</NavItem>
         <NavItem href='/work'>Work</NavItem>
         <NavItem href='/flash'>Flash</NavItem>
-        <NavItem href='/faq'>FAQ</NavItem>
-        <NavItem href='/guest'>Guest</NavItem>
       </ul>
     </nav>
   );
