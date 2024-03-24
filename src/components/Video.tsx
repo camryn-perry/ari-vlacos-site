e50e9d66a227f082a379c818e13be63c50dc1758
Camryn Perry <camrynpaigeperry@gmail.com>
2024-03-24 16:19:23 -0400

    video resizing

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -9,7 +9,7 @@ export type VideoProps = {
 export function Video({ src, title, role }: VideoProps) {
   return (
     <div>
-      <iframe src={src} frameBorder={'0'} allowFullScreen />
+      <iframe className='w-full aspect-video' src={src} frameBorder={'0'} allowFullScreen />
       <p>{title}</p>
       <p>{role}</p>
     </div>
