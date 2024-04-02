325220dcd536c36d8d3d34347398e1890454c5fd
Camryn Perry <camrynpaigeperry@gmail.com>
2024-04-01 20:19:48 -0400

    add proper image gallery

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,34 +1,34 @@
+'use client';
 import React from 'react';
 import clsx from 'clsx';
 import Image, { StaticImageData } from 'next/image';
+import { Gallery } from 'react-grid-gallery';
+import Lightbox from 'yet-another-react-lightbox';
 
-let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2'];
 const ImageGallery = ({ images }: { images: StaticImageData[] }) => {
+  // code reference:
+  // https://github.com/benhowell/react-grid-gallery/blob/master/examples/with-yet-another-react-lightbox/src/App.tsx
+  const slides = images.map(({ src, width, height }) => ({
+    src,
+    width,
+    height,
+  }));
+
+  const [index, setIndex] = React.useState(-1);
+
+  const handleClick = (index: number, item: StaticImageData) => setIndex(index);
   return (
-    <ul
-      role='list'
-      className='grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3'>
-      {images.map((image, imageIndex) => (
-        <li
-          key={image.src}
-          className={clsx(
-            'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
-            // `relative aspect-[${image.width}/${image.height}] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800`,
-            // rotations[imageIndex % rotations.length],
-          )}>
-          <a href={image.src}>
-            <Image
-              src={image}
-              alt=''
-              height={image.height / 2}
-              width={image.width / 2}
-              sizes='(min-width: 640px) 18rem, 11rem'
-              className='absolute inset-0 h-full w-full object-cover'
-            />
-          </a>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <Gallery
+        images={images}
+        margin={10}
+        rowHeight={360}
+        onClick={handleClick}
+        enableImageSelection={false}
+      />
+      {/* not working rn */}
+      {/* <Lightbox slides={slides} open={index >= 0} index={index} close={() => setIndex(-1)}/> */}
+    </div>
   );
 };
 export default ImageGallery;
